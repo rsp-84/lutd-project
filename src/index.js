6a40279e35a321b1a45a29716b33acad9da3feeb
rsp-84 <ryan.pflugradt@outlook.com>
2020-12-05 10:13:46 -0600

    apollo server setup with express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-console.log('Hello Project. Changed with nodemon! Added Babel-Node!');
+const app = express();
 
-console.log(process.env.MY_SECRET);
+app.use(cors());
+
+const schema = gql`
+    type Query {
+        me: User
+    }
+
+    type User {
+        id: Int,
+        username: String,
+        email: String
+
+    }
+`;
+const resolvers = {
+    Query: {
+        me: () => {
+            return {
+                id: 23,
+                username: 'Ryan',
+                email: 'test@test.com'
+            };
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql');
+});
