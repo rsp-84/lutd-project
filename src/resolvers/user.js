36bc2da9ac9c32087ed63f6a75b862878cb6234e
rsp-84 <ryan.pflugradt@outlook.com>
2020-12-06 11:32:30 -0600

    Apollo server authentication

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,27 +1,77 @@
+import jwt from 'jsonwebtoken';
+import { AuthenticationError, UserInputError } from 'apollo-server';
+const secret = process.env.SECRET;//TODO: why wasnt this coming from apollo context?
+const expiresIn = '30m';//
+
+const createToken = async (user) => {
+    const { id, email, username } = user;
+    return await jwt.sign({ id, email, username }, secret, {
+        expiresIn,
+    });
+};
+
 export default {
     Query: {
-      users: async (parent, args, { models }) => {
-        return await models.User.findAll();
-      },
-      user: async (parent, { id }, { models }) => {
-        return await models.User.findById(id);
-      },
-      me: async (parent, args, { models, me }) => {
-        if (!me) {
-            return null;
-          }
-        
-        return await models.User.findById(me.id);
-      },
+        users: async (parent, args, { models }) => {
+            return await models.User.findAll();
+        },
+        user: async (parent, { id }, { models }) => {
+            return await models.User.findById(id);
+        },
+        me: async (parent, args, { models, me }) => {
+            if (!me) {
+                return null;
+            }
+
+            return await models.User.findById(me.id);
+        },
     },
-  
+
+    Mutation: {
+        signUp: async (
+            parent,
+            { username, email, password },
+            { models, secret },
+        ) => {
+            const user = await models.User.create({
+                username,
+                email,
+                password,
+            });
+
+            return { token: createToken(user, secret, '30m') };
+        },
+
+        signIn: async (
+            parent,
+            { login, password },
+            { models, secret },
+        ) => {
+            const user = await models.User.findByLogin(login);
+
+            if (!user) {
+                throw new UserInputError(
+                    'No user found with this login credentials.',
+                );
+            }
+
+            const isValid = await user.validatePassword(password);
+
+            if (!isValid) {
+                throw new AuthenticationError('Invalid password.');
+            }
+
+            return { token: createToken(user, secret, '30m') };
+        },
+    },
+
     User: {
-      messages: async (user, args, { models }) => {
-        return await models.Message.findAll({
-          where: {
-            userId: user.id,
-          },
-        });
-      },
+        messages: async (user, args, { models }) => {
+            return await models.Message.findAll({
+                where: {
+                    userId: user.id,
+                },
+            });
+        },
     },
-  };
+};
